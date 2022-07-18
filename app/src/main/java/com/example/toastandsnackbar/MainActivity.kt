a7ddaf12aa3a67f57d2ec2f66ffc4591d05344e7
Muhammad Azzam <MazzamR08@gmail.com>
2022-07-18 14:55:33 +0700

    update toast

--- app/src/main/java/com/example/toastandsnackbar/MainActivity.kt
+++ app/src/main/java/com/example/toastandsnackbar/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.toastandsnackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.example.toastandsnackbar.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
 
-        binding.btnSnackbar.setOnClickListener {
+        /* binding.btnSnackbar.setOnClickListener {
             val snackBar = Snackbar.make(binding.root, "Appeared Successfully", Snackbar.LENGTH_INDEFINITE)
             //untuk menambahkan action
             snackBar.setAction("Retry"){
@@ -27,6 +28,20 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnToast.setOnClickListener {
             Toast.makeText(this, "Toast Clicked", Toast.LENGTH_SHORT).show()
+        }
+    }*/
+
+        binding.btnToast.setOnClickListener {
+            Toast(this).showCustomToast ("Hello! This is a custom Toast!", this)
+
+        }
+
+        binding.btnSnackbar.setOnClickListener {
+            val success = Snackbar.make(binding.btnSnackbar, "Success", Snackbar.LENGTH_LONG)
+                success.setAction("Cancel") {
+                    Toast.makeText(this, "Successfully", Toast.LENGTH_SHORT).show()
+                }
+                .show()
         }
     }
 }
